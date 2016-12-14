1dadb9ff8bc59778fe553c85b24c45c775c03b2c
luii <czarnetzki.philipp@protonmail.ch>
2016-12-14 19:41:20 +0100

    Add decrypt functionality

--- index.js
+++ index.js
@@ -23,3 +23,58 @@ const IV  = '9bc24cb995cb8db3'
  * @type {String}
  */
 const URL = 'http://service.jdownloader.org/dlcrypt/service.php?srcType=dlc&destType=pylo&data='
+
+/**
+ * The expression for encrypted key from JD.org
+ * @type {Regex}
+ */
+const RC_EXPRESSION = /<rc>([\s\S]+)<\/rc>/i
+
+/**
+ * Decrypt dlc container
+ * @todo   use maybe async/await or promises and clean up
+ * @return {[type]} [description]
+ */
+async function decrypt(file) {
+  let dlcKey = file.slice(-88) // get the last 88 chars (this is our key)
+  let dlcData = Buffer.from(file.slice(0, -88).toString(), 'base64') // the rest is the content
+
+  let rc = await request(`${URL}${dlcKey}`)
+  let strippedRc = Buffer.from(rc.match(RC_EXPRESSION)[1], 'base64')
+
+  let decryptedKey  = await aes_decrypt(strippedRc, KEY, IV, 0).catch((err) => console.log)
+  let decryptedData = await aes_decrypt(dlcData, decryptedKey, decryptedKey, 0).catch((err) => console.log)
+
+  return await parseXML(Buffer.from(decryptedData, 'base64'))
+}
+
+/**
+ * Decrypts the input data with the key and initializing vector
+ * @param  {String} data    The string to decrypt
+ * @param  {string} key     The key for decrypting
+ * @param  {string} iv      The init vector
+ * @param  {Number} padding The padding of decipher defaults to 0
+ * @return {String}         Returns a decrypted string (from binary to utf8)
+ */
+function aes_decrypt(data, key, iv, padding) {
+  let decipher = crypto.createDecipheriv('AES-128-CBC', key, iv)
+  decipher.setAutoPadding(padding || 0)
+  return Promise.resolve(decipher.update(data, 'binary', 'utf8'))
+}
+
+/**
+ * [description]
+ * @param  {[type]} buf [description]
+ * @return {[type]}       [description]
+ */
+function parseXML (buf) {
+  return new Promise(function(resolve, reject) {
+    xml.parseString(buf.toString('utf8'), function(err, result) {
+      if (!err) return resolve(result)
+      return reject(err)
+    })
+  })
+}
+
+// export public interface
+module.exports = decrypt
